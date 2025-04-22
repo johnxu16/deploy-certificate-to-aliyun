209fbddd8cf92c4a15a563b42503a5ad9155624c
johnxu16 <bringerxu@163.com>
2025-04-22 19:24:18 +0800

    refactor: add tccli

---
+++ src/set-cname.ts
@@ -0,0 +1,50 @@
+import Env from '@alicloud/darabonba-env';
+import 'zx/globals'
+import { parseStringPromise } from 'xml2js'
+
+(async () => {
+  const bucket = Env.getEnv("BUCKET")
+  const domain = Env.getEnv("DOMAIN")
+  const cdn_domain = Env.getEnv("ALIYUN_CDN_DOMAIN")
+  const sld_domain = domain.split('.').slice(-2).join('.')
+  const sub_domain = cdn_domain.replace(`.${sld_domain}`, '')
+
+  console.log(`domain: ${domain}`)
+  console.log(`cdn_domain: ${cdn_domain}`)
+  console.log(`sld_domain: ${sld_domain}`)
+  console.log(`sub_domain: ${sub_domain}`)
+
+  const createToken = await $`ossutil bucket-cname --method put --item token oss://${bucket} ${domain}`
+  if (createToken.exitCode !== 0) {
+    console.error(createToken.stderr)
+    return
+  }
+
+  const cnameTokenRes = await parseStringPromise(createToken.stdout)
+  const cnameToken = cnameTokenRes.CnameToken.Token[0]
+  const oss_url = `${bucket}.oss-cn-hangzhou.aliyuncs.com.`
+
+  // tencent 
+  // https://cloud.tencent.com/document/api/1427/112178
+  const addTokenRes = await $`tccli dnspod CreateTXTRecord --cli-unfold-argument --Domain ${sld_domain} --RecordLine 默认 --Value ${cnameToken} --SubDomain _dnsauth.${sub_domain}`
+  if (addTokenRes.exitCode !== 0) {
+    console.error(addTokenRes.stderr)
+    return
+  }
+
+  // https://cloud.tencent.com/document/api/1427/56180
+  const addCnameRes = await $`tccli dnspod CreateRecord --cli-unfold-argument --Domain ${sld_domain} --RecordType CNAME --RecordLine 默认 --Value ${oss_url} --SubDomain ${sub_domain}`
+  if (addCnameRes.exitCode !== 0) {
+    console.error(addCnameRes.stderr)
+    return
+  }
+
+  await $`sleep 120; echo waiting for 2 minutes`
+
+  const putCnameRes = await $`ossutil bucket-cname --method put --item certificate oss://${bucket} ~/certs/${domain}/oss_cname.xml`
+
+  if (putCnameRes.exitCode !== 0) {
+    console.error(putCnameRes.stderr)
+    return
+  }
+})()
